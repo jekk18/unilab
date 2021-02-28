256ea8707c89e25f5f1ec38b4a357ab6f39f1c0f
jekkk18 <79743397+jekkk18@users.noreply.github.com>
2021-02-28 19:15:26 +0400

    addWeblik

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ function burgerClick(){
 	var elem = document.getElementById("mob-nav");
 	var burgElem = document.getElementById("burger");
 	var clasTag = document.getElementById("section");
+	var avatarImg = document.getElementById("mob-avatar");
 	burgElem.classList.toggle("open");
 	elem.classList.toggle("open");
 	clasTag.classList.toggle("close");
+	avatarImg.classList.toggle("opacit");
+
 } 
